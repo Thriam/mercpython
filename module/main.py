655f6ad91ffcee784a1b59f77a0923dcc8da9520
Thriam <thriambala@gmail.com>
2026-01-14 09:26:26 +0530

    Added first module

---
+++ module/main.py
@@ -0,0 +1,14 @@
+"""
+Docstring for mercpython.module.main
+"""
+
+
+import math_utils
+
+from math_utils import square, plus2, plus3
+
+
+print(square(4))
+print(math_utils.cube(3))
+print(plus2(4))
+print(plus3(3))
---
+++ module/math_utils.py
@@ -0,0 +1,41 @@
+"""
+Docstring for mercpython.module.math_utils
+
+This provides various functions for mathematical operations
+"""
+
+
+def square(num):
+    """
+    Docstring for square
+
+    :param num: Takes input and returns the square of it
+    """
+    return num*num
+
+
+def cube(num):
+    """
+    Docstring for cube
+
+    :param num: Takes input and returns the cube of it
+    """
+    return num*num*num
+
+
+def plus2(num):
+    """
+    Docstring for plus2
+
+    :param num: Add 2 to the input
+    """
+    return num + 2
+
+
+def plus3(num):
+    """
+    Docstring for plus3
+
+    :param num: Add 3 to the input
+    """
+    return num + 3
